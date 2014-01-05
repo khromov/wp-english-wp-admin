77e8e5c4bc80bdee072fc89b34e2ceb748cc019a
khromov <stanislav.khromov@gmail.com>
2014-01-05 03:11:53 +0100

    - Initial commit, version 1.2

---
+++ english-wp-admin.php
@@ -0,0 +1,233 @@
+<?php
+/*
+Plugin Name: English WordPress Admin
+Plugin URI: http://wordpress.org/plugins/english-wp-admin
+Description: Lets users change their administration language to English
+Version: 1.2
+Author: khromov
+Author URI: http://snippets.khromov.se
+License: GPL2
+*/
+
+/*
+ * Main plugin class
+ */
+class Admin_Custom_Language
+{
+	/* Constructor for adding hooks */
+	function __construct()
+	{
+		//Locale filter
+		add_filter('locale', array(&$this, 'set_locale'));
+
+		//Registers GET listener to toggle setting
+		add_action('init', array(&$this, 'register_endpoints'));
+
+		//Adds admin bar menu
+		add_action('admin_bar_menu', array(&$this, 'admin_bar'), 31);
+		add_action('admin_head', array($this, 'admin_css'));
+
+		if($this->english_install_only())
+			add_action( 'admin_notices', array($this, 'admin_notices'));
+	}
+
+	/**
+	 * This function is responsible fo setting the locale via the locale filter
+	 *
+	 * @param $lang the current locale
+	 * @return string the locale that should be used
+	 */
+	function set_locale($lang)
+	{
+		//If cookie is set and enabled, and we are not doing frontend AJAX, we should switch the locale
+		if($this->english_admin_enabled() && !$this->request_is_frontend_ajax())
+		{
+			//Switch locale if we are on an admin page
+			if(is_admin())
+				return 'en_US';
+		}
+
+		//Default return
+		return $lang;
+	}
+
+	/**
+	 * This plugin listens for the GET variable that toggles the current setting
+	 */
+	function register_endpoints()
+	{
+		//We're in admin
+		if(is_admin())
+		{
+			//Is the GET variable set?
+			if(isset($_GET['admin_custom_language_toggle']))
+			{
+				//Cast variable
+				$cookie_value = intval($_GET['admin_custom_language_toggle']);
+
+				//Set cookie
+				$cookie_value === 1 ? $this->set_cookie(1) : $this->set_cookie(0);
+
+				//Redirect back to admin
+				wp_redirect(admin_url());
+			}
+		}
+	}
+
+	/**
+	 * Sets the cookie. (1 year expiry)
+	 */
+	function set_cookie($value = '1')
+	{
+		setcookie('wordpress_admin_default_language_'. COOKIEHASH, $value, strtotime('+1 year'), COOKIEPATH, COOKIE_DOMAIN, false);
+	}
+
+	/**
+	 * Check so that we are not doing a frontend AJAX request
+	 *
+	 * @return bool
+	 */
+	function request_is_frontend_ajax()
+	{
+		return defined('DOING_AJAX') && DOING_AJAX && false === strpos( wp_get_referer(), '/wp-admin/' );
+	}
+
+	/**
+	 * Checks if WordPress has a non-english language configured
+	 *
+	 * @return bool True if we don't have any additional language set in WPLANG
+	 */
+	function english_install_only()
+	{
+		if(defined('WPLANG'))
+		{
+			return (WPLANG === 'en_US' || WPLANG === '') ? true : false;
+		}
+		else
+			return true;
+	}
+
+	/**
+	 * Adds a menu item to the admin bar via the admin_bar_menu hook
+	 *
+	 * @param $wp_admin_bar The WP_Admin_Bar object
+	 */
+	function admin_bar($wp_admin_bar)
+	{
+		//We're in admin and this is not an english-only install
+		if(is_admin() && !$this->english_install_only() && apply_filters('english_wordpress_admin_show_admin_bar', true) === true)
+		{
+			//Sets up the toggle link
+			$toggle_href = admin_url('?admin_custom_language_toggle=' . ($this->english_admin_enabled() ? '0' : '1'));
+
+			$message_on = __('Switch to native', 'admin-custom-language');
+			$message_off = __('Switch to English', 'admin-custom-language');
+
+			//Add main menu
+			$main_bar = array(
+				'id' => 'admin-custom-language-icon',
+				'title' => $this->admin_bar_title(),
+				'href' => $toggle_href,
+				'meta' => array(
+					'class' => 'admin-custom-language-icon'
+				)
+			);
+
+			//Add sub menu
+			$main_bar_sub = array(
+				'id' => 'admin-custom-language-icon-submenu',
+				'title' => ($this->english_admin_enabled() ? $message_on : $message_off),
+				'href' => $toggle_href,
+				'parent' => 'admin-custom-language-icon'
+			);
+
+			$wp_admin_bar->add_node($main_bar);
+			$wp_admin_bar->add_node($main_bar_sub);
+
+		}
+
+	}
+
+	/**
+	 * Sets the admin bar title
+	 *
+	 * @return string
+	 */
+	function admin_bar_title()
+	{
+		return get_locale();
+	}
+
+	/**
+	 * Gets the cookie settin value, or null if there is no cookie set
+	 *
+	 * @return int|null
+	 */
+	function cookie_setting_value()
+	{
+		if(isset($_COOKIE['wordpress_admin_default_language_'. COOKIEHASH]))
+			return intval($_COOKIE['wordpress_admin_default_language_'. COOKIEHASH]);
+		else
+			return null;
+	}
+
+	/**
+	 * Checks if the functionality is enabled
+	 *
+	 * @return bool True if we should show the english admin
+	 */
+	function english_admin_enabled()
+	{
+		return $this->cookie_setting_value() === 1 ? true : false;
+	}
+
+	/**
+	 * Adds a little icon to the admin bar for later WordPress versions
+	 */
+	function admin_css()
+	{
+		if($this->wp_version_minimum('3.8'))
+		{
+			echo '
+			<style type="text/css">
+				#wpadminbar #wp-admin-bar-admin-custom-language-icon > .ab-item:before
+				{
+					/* admin globe - content:"\f319"; */
+					/* translate icon */
+					content:"\f326";
+					top: 2px;
+				}
+         	</style>
+         ';
+		}
+	}
+
+	/**
+	 * Version checker function
+	 *
+	 * @param $version The version we want to check against the current one
+	 * @return bool True if the current WP version is at least as new as $version
+	 */
+	function wp_version_minimum($version)
+	{
+		if (version_compare($version, get_bloginfo('version'), '>=' ))
+			return true;
+		else
+			return false;
+	}
+
+	/*
+	 * Handles admin notices
+	 */
+	function admin_notices()
+	{
+		?>
+		<div class="error">
+			<p><?php _e( "<strong>English Wordpress Admin Error:</strong> You only have English language installed. Please install another language before using this plugin. <a href='http://codex.wordpress.org/Installing_WordPress_in_Your_Language' target='_blank'>Read more (WordPress codex)</a>", 'admin-custom-language' ); ?></p>
+		</div>
+		<?php
+	}
+}
+
+/* Init plugin */
+$english_wordpress_admin_plugin = new Admin_Custom_Language();
---
+++ readme.txt
@@ -0,0 +1,88 @@
+=== English WordPress Admin ===
+Contributors: khromov
+Tags: english, wpml, multilanguage
+Requires at least: 3.5
+Tested up to: 3.8
+Stable tag: 1.2
+License: GPL2
+
+Lets users change their administration language to English
+
+== Description ==
+This plugin lets users change their administration language to native English (en_US locale).
+
+This is useful during site development and for people more accustomed to the english administration panel, even if your site
+is in another language. (The frontend will still use the native language.)
+
+This plugin is developer friendly and small (~100 lines of code). Check the FAQ for customization examples.
+
+**Usage**
+
+*Basic usage*
+
+Once you have installed and activated the plugin, navigate to any admin page and check the top admin bar. A button will
+display your current locale. If you click on it, the admin will change to English locale (en_US). To switch back,
+press the button again.
+
+== Requirements ==
+* PHP 5.3 or higher
+
+== Translations ==
+* None
+
+== Installation ==
+1. Upload the `english-wp-admin` folder to `/wp-content/plugins/`
+2. Activate the plugin (English WordPress Admin) through the 'Plugins' menu in WordPress
+3. Use the functionality via the admin bar
+
+== Frequently Asked Questions ==
+
+= Some plugins are still in the native language when switching to English =
+
+To fix this, move the file /wp-content/plugins/english-wp-admin/english-wp-admin.php to /wp-content/mu-plugins/
+
+This will ensure this plugin is loaded before all other plugins and that it sets the correct language.
+This is a WordPress restriction.
+
+= How do I prevent regular users from having the option of changing the admin language? =
+
+If you only want the first admin user to have this option, put this code in your themes function.php file:
+
+    /** Only allow the admin user to change the admin language **/
+    if(get_current_user_id() === 1) {
+        add_filter('english_wordpress_admin_show_admin_bar', '__return_true');
+    }
+    else {
+        add_filter('english_wordpress_admin_show_admin_bar', '__return_false');
+    }
+
+= How do I automatically enable this plugin for certain users? =
+
+Use the snippet below to have admins always use the admin page in english.
+
+    /** Enable the plugin automatically for admin users */
+    if(current_user_can('manage_options')) {
+        global $english_wordpress_admin_plugin;
+        $english_wordpress_admin_plugin->set_cookie(1);
+        add_filter('english_wordpress_admin_show_admin_bar', '__return_false');
+    }
+
+= This plugin does not solve my needs =
+
+This is a tiny plugin with a small mission. If you want better customization, check out the [Native Dashboard](http://wordpress.org/plugins/wp-native-dashboard/) plugin instead which has more functionality at the expense of a larger codebase. 
+
+== Screenshots ==
+
+1. The plugin admin bar
+
+== Changelog ==
+
+= 1.1 =
+* Fix notice level error when WPLANG is not defined
+* Added notification for sites that only have English language installed
+
+= 1.0 =
+* Initial release
+
+== TODO ==
+* Verifying compatibility with WPML is on the todo list.
